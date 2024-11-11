51720cd8324ef9f25fd933562f8022790118a0f3
Siriwat6687103 <siriwat.sak@student.mahidol.ac.th>
2024-11-11 05:15:45 +0000

    6687103_Lab13

--- test_zoo.py
+++ test_zoo.py
@@ -10,5 +10,19 @@ class TestZoo(unittest.TestCase):
        
     # Add your additional test cases here.
 
+    def test_schoolage_tickey_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(18), 100)
+    
+    def test_adult_tickey_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(55), 150)
+    
+    def test_oldage_tickey_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(80), 100)
+
+    def test_noage_tickey_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 'invalid')
+    
+
+
 if __name__ == '__main__':
     unittest.main()
